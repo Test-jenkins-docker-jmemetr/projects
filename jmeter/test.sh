5f7665c351a7363eaa8604a16cb8415f1bbeb258
Test-jenkins-docker-jmemetr <78961111+Test-jenkins-docker-jmemetr@users.noreply.github.com>
2021-02-17 15:15:24 +0200

    56

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-JMETER_HOME=opt/apache-jmeter-5.3/bin
+T_DIR=test_jmx
+
+R_DIR=${T_DIR}/report
+
+rm -rf ${R_DIR} > /dev/null 2>&1
+mkdir -p ${R_DIR}
+
+/bin/rm -f ${T_DIR}/SOAP_jmeter_IPR_finaly_4.jtl ${T_DIR}/jmeter.log > /dev/null 2>&1
 
 ./run.sh -Dlog_level.jmeter=DEBUG \
-	-n -t ${JMETER_HOME}/SOAP_jmeter_IPR_finaly_4.jmx -l SOAP_jmeter_IPR_finaly_4.jtl -j jmeter.log \
-	-e -o tmp
+	-n -t ${T_DIR}/SOAP_jmeter_IPR_finaly_4.jmx -l ${T_DIR}/SOAP_jmeter_IPR_finaly_4.jtl -j ${T_DIR}/jmeter.log \
+	-e -o ${R_DIR}
